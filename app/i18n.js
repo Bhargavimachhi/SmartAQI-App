baa07aa8de6fba626c7be58a31f85970a03296f2
Bhargavimachhi <140937880+Bhargavimachhi@users.noreply.github.com>
2025-08-07 15:52:50 +0530

    fixed i18n bug

--- app/i18n.js
+++ app/i18n.js
@@ -1,21 +1,19 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import HttpBackend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import en from '../public/locales/en.json';
+import hi from '../public/locales/hi.json';
 
-i18n
-  .use(HttpBackend) // Load translations using HTTP (e.g., fetch from /locales)
-  .use(LanguageDetector) // Detect user language (browser)
-  .use(initReactI18next) // Bind react-i18next to React
-  .init({
-    fallbackLng: 'en',
-    debug: true,
-    interpolation: {
-      escapeValue: false, // React already escapes
-    },
-    backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json', // Translation path
-    },
-  });
+i18n.use(initReactI18next).init({
+  compatibilityJSON: 'v3',
+  resources: {
+    en: { translation: en },
+    hi: { translation: hi },
+  },
+  lng: 'en',
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18n;
